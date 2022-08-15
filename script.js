e03ece66b971e1201c2d4863beb2016653c00997
koushik <koushik.dey@live.com>
2022-08-15 20:27:23 +0300

    ch1

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
+/*
+// Challlenge 1: Use the select method to select the body tag in the document. Then append an h1 tag to it, and add the text Data Visualization with D3 into the h1 element.
 
 const x = d3.select('body')
       .append('h1')
-      .text('Data Visualization with D3');    
+      .text('Data Visualization with D3');
+
+*/
