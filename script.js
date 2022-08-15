27c3ce6d2a322f4948ee7c20d8701a3f493a136c
koushik <koushik.dey@live.com>
2022-08-15 20:53:35 +0300

    chng

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 /*
+// Add Document Elements with D3
 // Challlenge 1: Use the select method to select the body tag in the document. Then append an h1 tag to it, and add the text Data Visualization with D3 into the h1 element.
 
 const x = d3.select('body')
@@ -9,9 +10,12 @@ const x = d3.select('body')
 */
 
 /*
+// Select a Group of Elements with D3
 // Challlenge 2: Select all of the li tags in the document, and change their text to the string list item by chaining the .text() method.
 
 const list = d3.selectAll('li')
                .text('list item')
 
 */
+
+
