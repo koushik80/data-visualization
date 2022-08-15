5aa1ea7c9da50966d97040ad7788fddf5726fc4f
koushik <koushik.dey@live.com>
2022-08-15 21:32:05 +0300

    work with data

--- script.js
+++ script.js
@@ -18,4 +18,16 @@ const list = d3.selectAll('li')
 
 */
 
+/*
+//Work with Data in D3
+// Challlenge 3: Select the body node, then select all h2 elements. Have D3 create and append an h2 tag for each item in the dataset array. The text in the h2 should say New Title. Your code should use the data() and enter() methods.
 
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+d3.select("body").selectAll("h2")
+      .data(dataset)
+      .enter()
+      .append("h2")
+      .text("New Title");
+
+*/
