89a9da07b39744578a647622b03642bb21082e59
koushik <koushik.dey@live.com>
2022-08-15 22:13:45 +0300

    inline-style

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ d3.select("body").selectAll("h2")
       .data(dataset)
       .enter()
       .append("h2")
-      .text("New Title");
+      .text("New Title")
 
 */
 
@@ -43,7 +43,25 @@ d3.select("body").selectAll("h2")
       .data(dataset)
       .enter()
       .append("h2")
-      .text((d) => `${d} USD`);
-      //.text((d) => d + ' USD');
+      .text((d) => `${d} USD`)
+      //.text((d) => d + ' USD')
+
+*/
+
+/*
+// Add Inline Styling to Elements
+
+// Challlenge 5: Add the style() method to the code in the editor to make all the displayed text have a font-family of verdana.
+
+
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+d3.select("body").selectAll("h2")
+      .data(dataset)
+      .enter()
+      .append("h2")
+      .text((d) => `${d} USD`)
+      //.text((d) => d + ' USD')
+      .style("font-family","verdana")
 
 */
