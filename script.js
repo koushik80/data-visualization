0617ab62a38d97007bf693c55b8dd5359824a548
koushik <koushik.dey@live.com>
2022-08-17 23:30:49 +0300

    Add a Tooltip to a D3 Element

--- script.js
+++ script.js
@@ -423,3 +423,41 @@ const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
        .attr("x", (d, i) => i * 30)
        .attr("y", (d, i) => h - (3 * d) - 3);
 */
+
+/*
+// Add a Tooltip to a D3 Element
+
+// Challlenge 20: Append a title element under each rect node. Then call the text() method with a callback function so the text displays the data value.
+
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+svg.selectAll("rect")
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 30)
+      .attr("y", (d, i) => h - 3 * d)
+      .attr("width", 25)
+      .attr("height", (d, i) => d * 3)
+      .attr("fill", "navy")
+      .attr("class", "bar")
+      .append("title")
+      .text((d) => d);
+
+    svg.selectAll("text")
+       .data(dataset)
+       .enter()
+       .append("text")
+       .text((d) => d)
+       .attr("x", (d, i) => i * 30)
+       .attr("y", (d, i) => h - (d * 3 + 3))
+
+*/
