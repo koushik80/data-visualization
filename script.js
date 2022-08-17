14513d2931da73ce2345e611b7bcce6efe11de36
koushik <koushik.dey@live.com>
2022-08-17 22:00:02 +0300

    Style D3 Labels

--- script.js
+++ script.js
@@ -348,7 +348,42 @@ const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
        .enter()
        .append("text")
        .attr("x", (d, i) => i * 30)
-       .attr("y", (d,i) => h - 3 * d - 3)
+       .attr("y", (d, i) => h - (3 * d) - 3)
        .text((d, i) => d)
 */
 
+/*
+// Style D3 Labels
+
+// Challlenge 18: Set the font-size of the text elements to 25px, and the color of the text to red.
+
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    svg.selectAll("rect")
+       .data(dataset)
+       .enter()
+       .append("rect")
+       .attr("x", (d, i) => i * 30)
+       .attr("y", (d, i) => h - 3 * d)
+       .attr("width", 25)
+       .attr("height", (d, i) => d * 3)
+       .attr("fill", "navy");
+
+    svg.selectAll("text")
+       .data(dataset)
+       .enter()
+       .append("text")
+       .text((d) => d)
+       .attr("x", (d, i) => i * 30)
+       .attr("y", (d, i) => h - (3 * d) - 3)
+       .style("font-size", "25px")
+       .attr("fill", "red")
+*/
