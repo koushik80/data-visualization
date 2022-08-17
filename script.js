75cb4c1ae0e38b74f072764a502d12cce7d31b45
koushik <koushik.dey@live.com>
2022-08-17 15:48:40 +0300

    shape with svg

--- script.js
+++ script.js
@@ -150,7 +150,29 @@ const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 
 const svg = d3.select("body")
       .append("svg")
-      .attr("width", w) //or .style("width",w
-      .attr("height", h) // or .style("height",h);
+      .attr("width", w) //or .style("width",w)
+      .attr("height", h) // or .style("height",h)
+
+*/
+
+/*
+// Display Shapes with SVG
+
+// // Challlenge 11: Add a rect shape to the svg using append(), and give it a width attribute of 25 and height attribute of 100. Also, give the rect x and y attributes each set to 0.
+
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h)
+                  .append("rect")
+                  .attr("width", 25)
+                  .attr("height", 100)
+                  .attr("x", 0)
+                  .attr("y", 0)
 
 */
