7f0c8accdb9b2dd3adb7d44cad30057a0a956294
koushik <koushik.dey@live.com>
2022-08-17 19:41:06 +0300

    Invert SVG Elements

--- script.js
+++ script.js
@@ -261,3 +261,31 @@ svg.selectAll("rect")
       .attr("width", 25)
       .attr("height", (d, i) => d * 3)
 */
+
+/*
+
+// Invert SVG Elements
+
+// Challlenge 15: Change the callback function for the y attribute to set the bars right-side-up. Remember that the height of the bar is 3 times the data value d.
+
+// Note: In general, the relationship is y = h - m * d, where m is the constant that scales the data points.
+
+const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+
+    const w = 500;
+    const h = 100;
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    svg.selectAll("rect")
+       .data(dataset)
+       .enter()
+       .append("rect")
+       .attr("x", (d, i) => i * 30)
+          .attr("y", (d, i) => h - 3 * d) 
+       .attr("width", 25)
+       .attr("height", (d, i) => 3 * d);
+*/
