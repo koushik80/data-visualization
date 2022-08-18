b038180284f9afc841c05fccc508b89d6f776adc
koushik <koushik.dey@live.com>
2022-08-19 00:53:38 +0300

    Render Images from Data Sources

--- JSON APIs & AJAX/script.js
+++ JSON APIs & AJAX/script.js
@@ -119,3 +119,29 @@ document.addEventListener('DOMContentLoaded', function(){
     };
   });
 */
+
+/*
+// Render Images from Data Sources
+// Challenge 7: Add code to use the imageLink and altText properties in an img tag.
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('getMessage').onclick = function(){
+      const req=new XMLHttpRequest();
+      req.open("GET",'/json/cats.json',true);
+      req.send();
+      req.onload = function(){
+        const json = JSON.parse(req.responseText);
+        let html = "";
+        json.forEach(function(val) {
+          html += "<div class = 'cat'>";
+          // Add your code below this line
+          html += "<img src = '" + val.imageLink + "' " + "alt='" + val.altText + "'>";
+          // Add your code above this line
+          html += "</div><br>";
+        });
+        document.getElementsByClassName('message')[0].innerHTML=html;
+      };
+     };
+  });
+  */
