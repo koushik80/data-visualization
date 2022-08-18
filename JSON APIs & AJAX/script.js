9e54610162874cf14d8ce73539e22909127e050c
koushik <koushik.dey@live.com>
2022-08-18 17:29:52 +0300

    Handle Click Events with JavaScript using the onclick property

---
+++ JSON APIs & AJAX/script.js
@@ -0,0 +1,12 @@
+
+/*
+// Handle Click Events with JavaScript using the onclick property
+
+// Challenge 1: Add a click event handler inside of the DOMContentLoaded function for the element with id of getMessage.
+
+document.addEventListener('DOMContentLoaded', function(){
+    // Add your code below this line
+document.getElementById('getMessage').onclick = function () {}
+    // Add your code above this line
+  });
+  */
