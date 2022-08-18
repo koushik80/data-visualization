047338ff599bc8620736a0069c47224d2592ddb7
koushik <koushik.dey@live.com>
2022-08-19 01:00:48 +0300

    Pre-filter JSON to Get the Data You Need

--- JSON APIs & AJAX/script.js
+++ JSON APIs & AJAX/script.js
@@ -145,3 +145,35 @@ document.addEventListener('DOMContentLoaded', function(){
      };
   });
   */
+
+  /*
+  // Pre-filter JSON to Get the Data You Need
+
+  // Challenge 8: Add code to filter the json data to remove the cat with the id value of 1.
+
+  document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('getMessage').onclick = function(){
+      const req = new XMLHttpRequest();
+      req.open("GET",'/json/cats.json', true);
+      req.send();
+      req.onload=function(){
+        let json = JSON.parse(req.responseText);
+        let html = "";
+        // Add your code below this line
+
+        json = json.filter(function(val) {
+  return (val.id !== 1);
+});
+        // Add your code above this line
+         json.forEach(function(val) {
+           html += "<div class = 'cat'>"
+
+           html += "<img src = '" + val.imageLink + "' " + "alt='" + val.altText + "'>"
+
+           html += "</div>"
+         });
+         document.getElementsByClassName('message')[0].innerHTML = html;
+       };
+     };
+  });
+  */
